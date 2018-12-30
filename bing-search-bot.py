4ee78bbf05b2f644f98d9822444ed7f4a6999346
Jishnu <33438589+jishnu1@users.noreply.github.com>
2018-12-30 18:11:54 -0500

    Add files via upload

--- bing-search-bot.py
+++ bing-search-bot.py
@@ -4,7 +4,7 @@ import time
 url = "https://www.bing.com/search?q="
 ch = 'A'
 
-for i in range(0,34):
+for i in range(0,30):
     webbrowser.open(url+ch, new=0)
     ch = chr(ord(ch) + 1)
     time.sleep(1)
