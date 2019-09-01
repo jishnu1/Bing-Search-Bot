eb03faae4a294af5aae2cfd68d0b147b4d4d84c0
Jishnu <33438589+jishnu1@users.noreply.github.com>
2019-09-01 16:34:10 -0400

    Add files via upload

--- bing-search-bot-mobile.py
+++ bing-search-bot-mobile.py
@@ -4,7 +4,10 @@ import time
 url = "https://www.bing.com/search?q="
 ch = 'A'
 # 100 pts
-for i in range(0, 21):
+for i in range(0, 20):
     webbrowser.open(url+ch, new=0)
+    # webbrowser.open(url+ch+ch, new=0)     | uncomment for days
+    # time.sleep(1.5)                       | with double points
     ch = chr(ord(ch) + 1)
     time.sleep(1.5)
+    
--- bing-search-bot-pc.py
+++ bing-search-bot-pc.py
@@ -6,7 +6,8 @@ ch = 'A'
 # 150 pts
 for i in range(0, 30):
     webbrowser.open(url+ch, new=0)
+    # webbrowser.open(url+ch+ch, new=0)     | uncomment for days
+    # time.sleep(1)                         | with double points
     ch = chr(ord(ch) + 1)
     time.sleep(1)
-    # webbrowser.open(url+ch+ch, new=0)
-    # time.sleep(1)
+    
